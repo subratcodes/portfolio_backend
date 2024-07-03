a39c164f9c8a73ec0d6ba1586031ed331ced9dac
Subrat Kumar Singh <subratsinghsks_cse16@its.edu.in>
2024-07-03 18:12:36 +0000

    baseline testing via Spring boot Mock MVC

--- src/main/java/portfolio/DemoApplication.java
+++ src/main/java/portfolio/DemoApplication.java
@@ -11,8 +11,6 @@ import portfolio.configs.EnvionmentConfigs;
 
 
 
-
-
 @EnableMethodSecurity(prePostEnabled = true, securedEnabled = true )
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
--- src/test/java/portfolio/DemoApplicationTests.java
+++ src/test/java/portfolio/DemoApplicationTests.java
@@ -1,24 +1,31 @@
 package portfolio;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.isA;
 
 import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import portfolio.Service.ShipwreckService;
 import portfolio.Service.ShipwreckServiceImpl;
 
 
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class DemoApplicationTests {
 
 
-
-	ShipwreckService service=new ShipwreckServiceImpl();
-
-
+	@Autowired
+	MockMvc mvc;
 
 
 	@Test
@@ -29,15 +36,26 @@ class DemoApplicationTests {
 	}
 
 	@Test
-	@DisplayName("shipwreck_service_count")
-	public void testCount(){
-		 long count=service.count();
+	@DisplayName("Testing a simple controller")
+	public void SimpleTest(){
 
-		 assertEquals(244,count);
-	
-	
-	
+
+		try {
+			mvc.perform(MockMvcRequestBuilders.get("/api/v1/productsV2?page=0")).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+			.andExpect((ResultMatcher) MockMvcResultMatchers.jsonPath("latdec",isA(Integer.class)));
+
+		
+		
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.toString());
+
 		}
+	}
+
+
+	
+
 
 
 
