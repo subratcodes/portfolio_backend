db5b08e35146456c16667618a732af7d7ce09599
Subrat Kumar Singh <subratsinghsks_cse16@its.edu.in>
2024-09-23 21:54:45 -0400

    added build plugins git and build

--- src/main/java/portfolio/Controllers/accountsCtr.java
+++ src/main/java/portfolio/Controllers/accountsCtr.java
@@ -2,14 +2,22 @@ package portfolio.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.Optional;
+
+import portfolio.Exceptions.NoDataFound;
 import portfolio.Models.Externals.accounts;
+import portfolio.Response.ResponseTemplate;
 import portfolio.Service.accountsService.accountServiceContract;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
@@ -25,6 +33,29 @@ public class accountsCtr {
     public Page<accounts> getAccounts(@RequestParam("page") int pageNo){
       return accountService.findAll(pageNo);
     }
+
+
+
+    @GetMapping("/account/{id}")
+    public ResponseEntity<ResponseTemplate> getMethodName(@PathVariable String id) throws Exception {
+
+        Optional<accounts> account=accountService.findById(id);
+
+
+        if(!account.isPresent()){
+          ResponseTemplate template=ResponseTemplate.builder()
+          .status(HttpStatus.OK).build();
+          return ResponseEntity.ok(template);
+        }
+
+
+        ResponseTemplate template=ResponseTemplate.builder()
+        .status(HttpStatus.OK).data(Arrays.asList(account.get())).build();
+        return ResponseEntity.ok(template);
+        
+      
+    }
+    
 
 
 
